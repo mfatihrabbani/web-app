7dd9b84e1a46d228f12bd15a93ca46f592bfeed6
mfatihrabbani <mfatihrabbani@gmail.com>
2022-10-22 23:13:39 +0700

    add state in register page

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,25 +1,56 @@
-function Register(){
-    
-    return (
-        <div className="auth-Form-container">
-      <form className="auth-form">
+import { useState } from "react";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmitRegister = (event) => {
+    event.preventDefault()
+    console.log(email + " " + password + " " + confirmPassword) 
+  }
+  
+  return (
+    <div className="auth-Form-container">
+      <form className="auth-form" onSubmit={handleSubmitRegister}>
         <div className="auth-content">
           <h3 className="auth-title">Sign In</h3>
           <div className="form-group mt-3">
             <label>Email Address</label>
-            <input type="email" className="form-control mt-1" placeholder="Email Address"/>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+            />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input type="password" className="form-control mt-1" placeholder="Password"/>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+            />
           </div>
           <div className="form-group mt-3">
             <label>Confirm Password</label>
-            <input type="password" className="form-control mt-1" placeholder="Confirm Password"/>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Confirm Password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value)
+              }}
+            />
           </div>
           <div className="d-grid gap-2 mt-3">
             <button className="btn btn-primary" type="submit">
-            Submit
+              Submit
             </button>
           </div>
           <p className="forgot-password text-right mt-2">
@@ -27,8 +58,8 @@ function Register(){
           </p>
         </div>
       </form>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default Register;
