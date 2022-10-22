882bb2c6392e9096906be51c5c3aaba1c320afc5
mfatihrabbani <mfatihrabbani@gmail.com>
2022-10-22 23:13:23 +0700

    add state in login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitLogin = (event) => {
+    event.preventDefault();
+    console.log(email + password);
+  };
 
   return (
     <div className="auth-Form-container">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmitLogin}>
         <div className="auth-content">
           <h3 className="auth-title">Sign In</h3>
           <div className="form-group mt-3">
             <label>Email Address</label>
-            <input type="email" className="form-control mt-1" placeholder="Email Address"/>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input type="password" className="form-control mt-1" placeholder="Password"/>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
           <div className="d-grid gap-2 mt-3">
             <button className="btn btn-primary" type="submit">
-            Submit
+              Submit
             </button>
           </div>
           <p className="forgot-password text-right mt-2">
